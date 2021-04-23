4f947e220e77b683683a7e15d2d364256066acf1
Khursheed Gaddi <chaudharyrc33@gmail.com>
2021-04-23 17:00:26 +0530

    ListView, StateManagement Completed

---
+++ lib/MOCK_DATA.dart
@@ -0,0 +1,98 @@
+const MOCK_DATA = [
+  {
+    "username": "jrenouf0",
+    "profileImage":
+        "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments":
+        "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo.",
+    "likes": 10165,
+    "postImage":
+        "https://images.unsplash.com/photo-1611028086947-c405be3d94d6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8aW1hZ2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "hfedynski1",
+    "profileImage":
+        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cG9ydHJhaXR8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments":
+        "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.",
+    "likes": 10217,
+    "postImage":
+        "https://images.unsplash.com/photo-1598970708695-edae094786cc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "bsharphurst2",
+    "profileImage":
+        "https://images.unsplash.com/photo-1521227889351-bf6f5b2e4e37?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cG9ydHJhaXR8ZW58MHwwfDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments":
+        "Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+    "likes": 10300,
+    "postImage":
+        "https://images.unsplash.com/photo-1551708919-cc12ce6421eb?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "kwayte3",
+    "profileImage":
+        "https://images.unsplash.com/photo-1528914457842-1af67b57139d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments":
+        "Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia.",
+    "likes": 1088,
+    "postImage":
+        "https://images.unsplash.com/photo-1587279063133-e65e2d8b849f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "rkinkade4",
+    "profileImage":
+        "https://images.unsplash.com/photo-1544817747-b11e3e3b6ac2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments": "Sed sagittis.",
+    "likes": 1050,
+    "postImage":
+        "https://images.unsplash.com/photo-1571205163089-0c192f8ff5ce?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "sstother5",
+    "profileImage":
+        "https://images.unsplash.com/photo-1512316609839-ce289d3eba0a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments":
+        "Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.",
+    "likes": 1036,
+    "postImage":
+        "https://images.unsplash.com/photo-1610789229959-3d9abd85a3b0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "lmctrustrie6",
+    "profileImage":
+        "https://images.unsplash.com/photo-1568364419615-88f850d5e170?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments": "Duis aliquam convallis nunc.",
+    "likes": 10237,
+    "postImage":
+        "https://images.unsplash.com/photo-1496285705189-c290050257f1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "jgregan7",
+    "profileImage":
+        "https://images.unsplash.com/photo-1558730234-d8b2281b0d00?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzd8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments": "Donec posuere metus vitae ipsum.",
+    "likes": 10328,
+    "postImage":
+        "https://images.unsplash.com/photo-1523476690916-d3c6bbfe74d3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjB8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "omumbray8",
+    "profileImage":
+        "https://images.unsplash.com/photo-1536611908954-016795a9d9a1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments": "Proin at turpis a pede posuere nonummy.",
+    "likes": 12309,
+    "postImage":
+        "https://images.unsplash.com/photo-1542693209-f2b4ed97f70d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fGltYWdlc3xlbnwwfDB8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    "username": "kpregel9",
+    "profileImage":
+        "https://images.unsplash.com/photo-1541040550327-d23059d44cd2?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTN8fHBvcnRyYWl0fGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    "comments":
+        "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.",
+    "likes": 1130,
+    "postImage":
+        "https://images.unsplash.com/photo-1544894079-4184d7a29169?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2VzfGVufDB8MHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  }
+];
